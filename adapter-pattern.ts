d900d44d06c507dc01490ed0a60f74906b6f611c
João Fernandes <joaovitorfernandes2@gmail.com>
2018-09-10 19:53:20 -0300

    Add adapter pattern

---
+++ adapter-pattern.ts
@@ -0,0 +1,77 @@
+const rand = n => Math.round(Math.random() * n);
+
+interface EnemyAttacker {
+  fireWeapon(): void;
+  driveForward(): void;
+  assignDriver(driverName: string): void;
+}
+
+// Target
+class EnemyTank implements EnemyAttacker {
+  public fireWeapon(): void {
+    const attackDamage: number = rand(10);
+    console.log(`Enemy Tank Does ${attackDamage} damage`);
+  }
+  public driveForward(): void {
+    const movement: number = rand(5);
+    console.log(`Enemy tank moves ${movement} spaces`);
+  }
+  public assignDriver(driverName: string): void {
+    console.log(`${driverName} is driving the tank`);
+  }
+}
+
+// Adaptee
+class EnemyRobot {
+  public smashWithHands(): void {
+    const attackDamage: number = rand(10);
+    console.log(`Enemy robot causes ${attackDamage} damage with its hands`);
+  }
+  public walkForward(): void {
+    const movement = rand(5);
+    console.log(`Enemy Robot Walks Forward ${movement} spaces`);
+  }
+  public reactToHuman(humanName: string): void {
+    console.log(`Enemy Robot Tramps on ${humanName}`);
+  }
+}
+
+class EnemyRobotAdapter implements EnemyAttacker {
+  private enemyRobot: EnemyRobot;
+  public constructor(enemyRobot: EnemyRobot) {
+    this.enemyRobot = enemyRobot;
+  }
+
+  public fireWeapon(): void {
+    this.enemyRobot.smashWithHands();
+  }
+
+  public driveForward(): void {
+    this.enemyRobot.walkForward();
+  }
+
+  public assignDriver(driverName: string): void {
+    this.enemyRobot.reactToHuman(driverName);
+  }
+}
+
+//---------------------------------------------------------------------------
+
+const rx7Tank: EnemyTank = new EnemyTank();
+const fredTheRobot: EnemyRobot = new EnemyRobot();
+const robotAdapter: EnemyRobotAdapter = new EnemyRobotAdapter(fredTheRobot);
+
+console.log("The Robot");
+fredTheRobot.reactToHuman("João");
+fredTheRobot.walkForward();
+fredTheRobot.smashWithHands();
+
+console.log("\nThe enemy tank");
+rx7Tank.assignDriver("Vitor");
+rx7Tank.driveForward();
+rx7Tank.fireWeapon();
+
+console.log("\nThe robot with adapter");
+robotAdapter.assignDriver("Fernandes");
+robotAdapter.driveForward();
+robotAdapter.fireWeapon();
