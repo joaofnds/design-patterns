4b48290e13c685351462c1c9d37c1e3733a56942
João Fernandes <joaovitorfernandes2@gmail.com>
2018-09-12 13:13:21 -0300

    Add bridge pattern

---
+++ bridge-pattern.ts
@@ -0,0 +1,88 @@
+abstract class EntertainmentDevice {
+  protected deviceState: number;
+  protected maxSetting: number;
+  protected volumeLevel: number = 0;
+
+  public abstract buttonFivePressed(): void;
+  public abstract buttonSixPressed(): void;
+
+  public deviceFeedback(): void {
+    if (this.deviceState > this.maxSetting || this.deviceState < 0) {
+      this.deviceState = 0;
+      console.log(`On: ${this.deviceFeedback}`);
+    }
+  }
+
+  public buttonSevenPressed(): void {
+    this.volumeLevel++;
+    console.log(`Volume at: ${this.volumeLevel}`);
+  }
+
+  public buttonEightPressed(): void {
+    this.volumeLevel--;
+    console.log(`Volume at: ${this.volumeLevel}`);
+  }
+}
+
+class TVDevice extends EntertainmentDevice {
+  public constructor(newDeviceState: number, newMaxSetting: number) {
+    super();
+    this.deviceState = newDeviceState;
+    this.maxSetting = newMaxSetting;
+  }
+  public buttonFivePressed(): void {
+    console.log("Channel Down");
+    this.deviceState--;
+  }
+  public buttonSixPressed(): void {
+    console.log("Channel Up");
+    this.deviceState++;
+  }
+}
+
+abstract class RemoteButton {
+  private theDevice: EntertainmentDevice;
+
+  public constructor(newDevice: EntertainmentDevice) {
+    this.theDevice = newDevice;
+  }
+
+  public buttonFivePressed(): void {
+    this.theDevice.buttonFivePressed();
+  }
+
+  public buttonSixPressed(): void {
+    this.theDevice.buttonSixPressed();
+  }
+
+  public abstract buttonNinePressed();
+}
+
+class TVRemoveMute extends RemoteButton {
+  public constructor(newDevice: EntertainmentDevice) {
+    super(newDevice);
+  }
+
+  public buttonNinePressed() {
+    console.log("TV Muted");
+  }
+}
+
+class TVRemovePause extends RemoteButton {
+  public constructor(newDevice: EntertainmentDevice) {
+    super(newDevice);
+  }
+
+  public buttonNinePressed() {
+    console.log("TV paused");
+  }
+}
+
+//----------------------------------------------------------------------
+const theTV: RemoteButton = new TVRemoveMute(new TVDevice(1, 200));
+const theTV2: RemoteButton = new TVRemovePause(new TVDevice(1, 200));
+theTV.buttonFivePressed();
+theTV.buttonSixPressed();
+theTV.buttonNinePressed();
+
+theTV2.buttonNinePressed();
