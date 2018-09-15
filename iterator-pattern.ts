f3dbeaf69644fb3a3352aa7cf89bd44b22889e0e
João Fernandes <joaovitorfernandes2@gmail.com>
2018-09-14 22:12:37 -0300

    Add iterator pattern (just comments)

---
+++ iterator-pattern.ts
@@ -0,0 +1,6 @@
+/*
+ * The iterator pattern provides you with a uniform way to access different collections of Object
+ * If you get an Array, ArrayList and Hashtable of Object, you pop out an iterator for each and treat them the same
+ * This provides a uniform way to cycle through different collections
+ * You can also write polymorphic code because you can refer to each collection of objects because they'll implement the same interface
+ */
