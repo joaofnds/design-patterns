8042136399b0a9af5094067e005d8a6dbf602b0d
João Fernandes <joaovitorfernandes2@gmail.com>
2018-10-22 23:42:10 -0300

    Add flyweight pattern (just comments)

---
+++ flyweight-pattern.ts
@@ -0,0 +1,9 @@
+/**
+ * What is the flyweight design pattern?
+ *
+ * - Used when you need to create a large number of similar objects
+ * - To reduce memory usage you share Object that are similar in some way
+ *   rather than creating new ones
+ *
+ * @see http://www.newthinktank.com/2012/10/flyweight-design-pattern-tutorial/
+ */
