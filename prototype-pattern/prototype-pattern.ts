3910a7c619fc61d64641eb532a0e5e221a495b8c
João Fernandes <joaofnds@joaofnds.com>
2022-03-19 14:33:43 -0300

    [ts] refactor prototype

--- prototype-pattern/prototype-pattern.ts
+++ prototype-pattern/prototype-pattern.ts
@@ -16,39 +16,33 @@ interface Animal {
 }
 
 class Sheep implements Animal {
-  private name: string;
+  name: string;
 
   public constructor() {
     console.log("Sheep is made");
   }
 
-  public makeCopy(): Animal {
+  makeCopy(): Animal {
     console.log("Sheep is being made");
     const sheepObject: Sheep = Object.create(this);
     return sheepObject;
   }
-
-  public setName(name: string): void {
-    this.name = name;
-  }
-
-  public getName(): string {
-    return this.name;
-  }
 }
 
 class CloneFactory {
-  public getClone(animalSample: Animal): Animal {
+  getClone(animalSample: Animal): Animal {
     return animalSample.makeCopy();
   }
 }
 
 //-------------------------------------------------
 
-const animalMaker: CloneFactory = new CloneFactory();
-const sally: Sheep = new Sheep();
-sally.setName("sally");
-const clonedSheep: Sheep = <Sheep>animalMaker.getClone(sally);
-clonedSheep.setName("sally clone");
-console.log(sally.getName());
-console.log(clonedSheep.getName());
+const animalMaker = new CloneFactory();
+const sally = new Sheep();
+sally.name = "sally";
+
+const clonedSheep = <Sheep>animalMaker.getClone(sally);
+clonedSheep.name = "sally clone";
+
+console.log(sally.name);
+console.log(clonedSheep.name);
